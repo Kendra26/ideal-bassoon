fc318c36b8be2c08044f52bb71d74e53fd065fe0
Kendra26 <115176410+Kendra26@users.noreply.github.com>
2025-07-02 13:27:36 -0500

    bitrise.yml

--- Terraform.tf
+++ Terraform.tf
@@ -1,47 +1,45 @@
-terraform {
-  cloud {
-    organization = "AcmeKen"
+format_version: '11'
+default_step_lib_source: https://github.com/bitrise-io/bitrise-steplib.git
+project_type: bash
 
-    workspaces {
-      name = "torrent-workspace"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "torrent_vm" {
-  ami           = "ami-0c94855ba95c71c99"
-  instance_type = "t3.micro"
-
-  user_data = <<-EOF
-    #!/bin/bash
-    # Install dependencies
-    sudo yum update -y
-    sudo yum install -y wget curl nodejs npm
-
-    # Install webtorrent-cli
-    npm install -g webtorrent-cli
-    mkdir downloads 
-
-    # Download torrent content
-    webtorrent download "magnet:?xt=urn:btih:FAF584A93A877E45EB83D98DDFEAF5AF3A010EC1&dn=Wang%20L.%20Control%20of%20Heavy%20Metals%20in%20the%20Environment%20Vol%202.%20Advanced%20Methods..2025&tr=udp://tracker.bittor.pw:1337/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://tracker.dler.org:6969/announce&tr=udp://open.stealth.si:80/announce&tr=udp://tracker.torrent.eu.org:451/announce&tr=udp://exodus.desync.com:6969/announce&tr=udp://open.demonii.com:1337/announce" --out downloads
-
-    # Upload dummy file with brackets
-    FILE="/home/ec2-user/dummy_files/My Love [EZTVx.to].mkv"
-    mkdir -p /home/ec2-user/dummy_files
-    echo "This is a dummy file" > "$FILE"
-    curl -T "$FILE" -u :1627383993 https://pixeldrain.com/api/file/
-
-    # Upload all downloaded files
-    find /downloads -type f | while read -r file; do
-      curl -T "$file" -u :26278288282828 https://pixeldrain.com/api/file/
-    done
-  EOF
-
-  tags = {
-    Name = "torrent-cloud-instance"
-  }
-}
+workflows:
+  torrent-download-upload:
+    steps:
+      - script@1:
+          name: Checkout code
+          run_if: |  
+            true
+      - script@1:
+          name: Install dependencies
+          run: |
+            # Install webtorrent-cli
+            npm install -g webtorrent-cli
+      - script@1:
+          name: Create dummy file with brackets in name
+          run: |
+            mkdir -p ./dummy_files
+            echo "This is a dummy file" > "./dummy_files/My Love [EZTVx.to].mkv"
+      - script@1:
+          name: Download torrent content with webtorrent-cli
+          run: |
+            webtorrent download "magnet:?xt=urn:btih:FAF584A93A877E45EB83D98DDFEAF5AF3A010EC1&dn=Wang%20L.%20Control%20of%20Heavy%20Metals%20in%20the%20Environment%20Vol%202.%20Advanced%20Methods..2025&tr=udp://tracker.bittor.pw:1337/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://tracker.dler.org:6969/announce&tr=udp://open.stealth.si:80/announce&tr=udp://tracker.torrent.eu.org:451/announce&tr=udp://exodus.desync.com:6969/announce&tr=udp://open.demonii.com:1337/announce" --out ./downloaded_files
+      - script@1:
+          name: Upload dummy file with brackets
+          run: |
+            file="./dummy_files/My Love [EZTVx.to].mkv"
+            filename=$(basename "$file")
+            echo "Uploading $filename..."
+            curl -T "$file" -u :2828287337 "https://pixeldrain.com/api/file/"
+      - script@1:
+          name: Upload all downloaded files (escape brackets during upload)
+          run: |
+            find ./downloaded_files -type f | while read -r file; do
+              filename=$(basename "$file")
+              escaped_filename="$filename"
+              if [[ "$filename" == *"["* || "$filename" == *"]"* ]]; then
+                escaped_filename="${filename//\[/\\[}"
+                escaped_filename="${escaped_filename//\]/\\]}"
+              fi
+              echo "Uploading $filename..."
+              curl -T "$file" -u :2929288374 "https://pixeldrain.com/api/file/"
+            done
