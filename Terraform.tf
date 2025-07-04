aeabe65318837ddc9895878188db5809036e27c2
Kendra26 <115176410+Kendra26@users.noreply.github.com>
2025-07-04 16:57:13 -0500

    Update Terraform.tf

--- Terraform.tf
+++ Terraform.tf
@@ -25,6 +25,7 @@ resource "null_resource" "webtorrent_pixeldrain_upload" {
 #!/bin/bash
 set -euo pipefail
 
+# Ensure necessary dependencies are installed
 if ! command -v webtorrent > /dev/null 2>&1; then
   echo "Installing webtorrent-cli..."
   npm install -g webtorrent-cli
@@ -35,12 +36,15 @@ if ! command -v jq > /dev/null 2>&1; then
   sudo apt-get update -y && sudo apt-get install -y jq
 fi
 
-mkdir -p "download"
+mkdir -p download
 
-echo "Downloading Terraform via webtorrent-cli from $TERRAFORM_TORRENT_URI"
+echo "Downloading Terraform via webtorrent-cli from magnet link"
 webtorrent download "magnet:?xt=urn:btih:9187d47205935b83f5ed046ff03a19870658d187&dn=The.Land.Before.Time.VI.The.Secret.Of.Saurus.Rock.1998.720p.WEBRip.x264-%5BYTS.AM%5D.mp4&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce" --out download
 
-TF_ZIP=$(ls *.zip | head -n 1)
+cd download
+
+TF_ZIP=$(ls *.zip 2>/dev/null | head -n 1)
+
 if [ -z "$TF_ZIP" ]; then
   echo "Terraform zip file not found!"
   exit 1
@@ -53,4 +57,7 @@ echo "Terraform binary contents:"
 ls -l terraform
 
 cd "${path.module}"
+EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
 }
